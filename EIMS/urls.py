ce07c325ae5a6754bd80af0e671b6daa58b88319
ssenyonga-personal <samuelssenyonga301@gmail.com>
2023-11-03 19:55:30 +0300

    Push 4

--- EIMS/urls.py
+++ EIMS/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
+from .views import ExpenditureinDetail
 
 urlpatterns = [
     path('', views.signin, name='signin'),
-    path('/dashboard', views.dashboard, name='dashboard'),
+    path('dashboard', views.dashboard, name='dashboard'),
+    path('specific-expenditure/<str:pk>/', ExpenditureinDetail.as_view(), name = 'exp-details'),
+    path('update-expenditure/<str:pk>/', UpdateExp.as_view(), name = 'update-exp'),
+    path('create-expenditure/<str:pk>/', CreateExp.as_view(), name = 'create-exp'),
+    path('all-expenditure-records', views.allExp, name=''),
+    path('signout', views.signout, name='signout'),
+    path('allExp-records', views.allExpRecords, name='allExp-records'),    
 ]
--- EIMS/views.py
+++ EIMS/views.py
@@ -7,13 +7,60 @@ from django.contrib.messages.views import SuccessMessageMixin
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib import messages
 from .models import Expenditure
+from django.contrib.auth import login, logout
 
 def signin(request):
-    return render(request, 'accounts/in.html')
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+
+        myUser = authenticate(email=email, password=password)
+
+        if myUser is not None:
+            login(request, myUser)
+            messages.success(request, 'Successfully Logged In...!')
+            return redirect('home')
+
+        else:
+            messages.error(request, 'Wrong Credentials...!')
+            return redirect('signin')
+
+    context = {
+        messages: 'messages',
+    }
+    return render(request, 'accounts/in.html', context)
 
 def dashboard(request):
     return render(request, 'app_pages/dashboard.html')
 
-class ExpenditureinDetails(DetailView):
+class ExpenditureinDetail(DetailView):
     model = Expenditure
     template_name = 'app_pages/expenditure-in-details.html'
+
+class CreateExp(SuccessMessageMixin, CreateView):
+    model = Expenditure
+    form_class = ExpenditureForm
+    template_name = "app_pages/new-exp.html"
+
+    success_message = "Expenditure was added successfully"
+
+    def form_valid(self, ExpenditureForm):
+        ExpenditureForm.instance.uploaded_by = self.request.user
+        return super().form_valid(ExpenditureForm)
+
+class UpdateExp(SuccessMessageMixin, UpdateView):
+    model = Expenditure
+    template_name = 'app_pages/update_exp.html'
+    # fields = ['Job_type','job_title', 'job_branch', 'uploaded_on', 
+    # 'deadline', 'description', 'qualifications']
+
+    success_message = "Expenditure was updated successfully"
+
+def allExpRecords(request):
+    return render(request, 'app_pages/allexp-records.html')
+
+
+def signout(request):
+    logout(request)
+    messages.success(request, 'Logged out successfully...!')
+    return redirect('signin')
