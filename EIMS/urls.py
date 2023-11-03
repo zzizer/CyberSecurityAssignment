9b8c8bd3128c7b8875a84a30efe038793318c058
ssenyonga-personal <samuelssenyonga301@gmail.com>
2023-11-03 19:29:20 +0300

    push2

--- EIMS/urls.py
+++ EIMS/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.signin, name='signin'),
+    path('/dashboard', views.dashboard, name='dashboard'),
 ]
--- EIMS/views.py
+++ EIMS/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import (
+    ListView, CreateView, DetailView, UpdateView, DeleteView
+)
+from django.contrib.auth.decorators import login_required
+from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib import messages
+from .models import Expenditure
 
 def signin(request):
     return render(request, 'accounts/in.html')
+
+def dashboard(request):
+    return render(request, 'app_pages/dashboard.html')
+
+class ExpenditureinDetails(DetailView):
+    model = Expenditure
+    template_name = 'app_pages/expenditure-in-details.html'
