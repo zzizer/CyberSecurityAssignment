19ca53ced1291e3e1b61e3185e0ae2ab5e2f93ac
SamuelSsenyonga <samuelssenyonga301@gmail.com>
2023-11-09 11:58:35 +0300

    now

--- EIMS/views.py
+++ EIMS/views.py
@@ -18,7 +18,7 @@ from django.core.exceptions import ValidationError
 from django.contrib.auth.views import PasswordChangeView, PasswordChangeForm
 from .forms import SetNewPasswordForm, PasswordResetForm
 from django.contrib.auth.tokens import default_token_generator
-from django.utils.encoding import force_text
+# from django.utils.encoding import force_text
 from django.utils.http import urlsafe_base64_decode
 from django.contrib.auth.tokens import default_token_generator
 from django.utils.encoding import force_bytes
@@ -26,6 +26,10 @@ from django.utils.http import urlsafe_base64_encode
 from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.conf import settings
+from django.utils.encoding import force_str
+from django.contrib.auth.tokens import default_token_generator
+# from django.utils.encoding import force_str
+from django.utils.http import urlsafe_base64_decode
 
 @login_required
 def set_new_password(request):
@@ -42,14 +46,14 @@ def set_new_password(request):
 
 def password_reset_confirm(request, uidb64, token):
     try:
-        uid = force_text(urlsafe_base64_decode(uidb64))
+        uid = force_str(urlsafe_base64_decode(uidb64))
         user = NewUser.objects.get(pk=uid)
 
         # Check if the token is valid
         if default_token_generator.check_token(user, token):
             # Log the user in and redirect to the password reset form
             login(request, user)
-            return redirect('set-new-password')
+            return redirect('set_new_password')
         else:
             messages.error(request, 'Invalid or expired token.')
             return redirect('login')
