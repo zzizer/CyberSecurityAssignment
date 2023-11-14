45960a010c486c373cea603d03d229cc48b242f5
samuel-ssenyonga <samuelssenyonga301@gmail.com>
2023-11-14 07:50:35 -0800

    Final

--- core/settings.py
+++ core/settings.py
@@ -86,22 +86,22 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'africonnect',
-#         'USER': 'africonnect',
-#         'PASSWORD': 'abc123ABC.',
-#         'PORT': '5432',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'cyber_security',
+        'USER': 'postgres',
+        'PASSWORD': 'abc123ABC.',
+        'PORT': '5432',
+    }
+}
 
 
 # Password validation
