189971403e984a8cd77fae3e30517baf291158c0
samuel-ssenyonga <samuelssenyonga301@gmail.com>
2023-11-17 04:55:53 -0800

    kanye

--- EIMS/models.py
+++ EIMS/models.py
@@ -120,7 +120,7 @@ class NewUser(AbstractBaseUser, PermissionsMixin):
                 raise ValidationError("Cannot use the same password as one of the last 10 passwords.")
     
 
-    expiration_days = 5  # Adjust this based on your requirements
+    expiration_days = 90  # Adjust this based on your requirements
 
     @property
     def days_until_password_expires(self):
--- core/settings.py
+++ core/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d^d*dxbox+29i!_bu3hcg5rd5g8-ip9!tvooj*u=ykt6+s!-8u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
